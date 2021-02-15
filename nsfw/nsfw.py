f395dac71f0954dbeb179298001fcd7e179582b1
krompert <h3llu@me.com>
2021-02-15 13:12:40 -0500

    Update nsfw.py

--- nsfw/nsfw.py
+++ nsfw/nsfw.py
@@ -434,8 +434,8 @@ class Nsfw(Core):
     @nsfwcheck()
     @commands.bot_has_permissions(embed_links=True)
     @commands.cooldown(1, 0.5, commands.BucketType.user)
-    @commands.command(aliases=["celebs"])
-    async def celebs(self, ctx: commands.Context):
+    @commands.command(aliases=["celebss"])
+    async def celebss(self, ctx: commands.Context):
         """Show some Celebrities images from random subreddits."""
 
-        await self._send_msg(ctx, _("celebs"), sub=sub.CELEBS)
+        await self._send_msg(ctx, _("celebs"), sub=sub.CELEBSS)
