8df82023a64ce604bbc4434857a92ff6e082eb8f
krompert <h3llu@me.com>
2021-12-27 17:39:49 -0500

    Update ticket.py

--- ticket/ticket.py
+++ ticket/ticket.py
@@ -302,7 +302,7 @@ class TicketSystem(commands.Cog):
         filepath = "var/www/html/"
         transcript = await chat_exporter.export(channel, channel.guild)
         fileNAME = filepath + f"{channel.name}.html"
-        fileNAME2 = + f"{channel.name}.html"
+        fileNAME2 = '/' + f"{channel.name}.html"
         if transcript is not None:
             outfile = open(fileNAME, "w", encoding="utf-8")
             outfile.write(transcript)
